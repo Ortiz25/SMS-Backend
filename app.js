5ade8b5ebd2f181136d79f48be369881616603b1
Samuel Deya <samueldeya@gmail.com>
2025-03-07 14:47:04 +0300

    first commit

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('trust proxy', true); 
+app.set('trust proxy', 1);
+ 
 
 // Rate limiting
 const limiter = rateLimit({
